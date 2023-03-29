678ce30317763dd47274058d23ae93c01c9b0b18
Murwan Eisa <eisa.murwan@gmail.com>
2023-03-29 18:03:40 +0200

    adding the jwt

--- app.js
+++ app.js
@@ -10,10 +10,77 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-/* const filePath = path.join(__dirname, "./views");
-app.set("views", filePath);
-console.log("first", filePath); */
 const PORT = 3000;
+const currentTaken = "";
+const currentPassword = "";
+
+app.get("/", (req, res) => {
+  res.render("identify.ejs");
+});
+
+app.get("/identify", (req, res) => {
+  res.render("identify.ejs");
+});
+app.get("/student1", (req, res) => {
+  res.render("student1.ejs");
+});
+app.get("/student2", (req, res) => {
+  res.render("student2.ejs");
+});
+app.get("/teacher", (req, res) => {
+  res.render("teacher.ejs");
+});
+
+app.post("/identify", (req, res) => {
+  const userPass = req.body.password;
+  const token = jwt.sign(userPass, process.env.token);
+  console.log("token", token);
+  /*  currentPassword = userPass;
+  res.redirect("start.ejs"); */
+});
+
+app.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    if (req.body !== "") {
+      //encrypt the password
+      const success = await db.getUser(username);
+      if (success) {
+        //if user is found
+        if (await bcrypt.compare(password, success.password)) {
+          console.log("true");
+          res.render("home.ejs");
+          var token = jwt.sign("username", process.env.token);
+          console.log(token);
+        } else {
+          console.log("false");
+          res.render("wrongPass.ejs");
+        }
+      }
+    }
+  } catch (error) {
+    res.render("error.ejs");
+  }
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, role, password } = req.body;
+  console.log(username, role, password);
+  try {
+    if (req.body !== "") {
+      //encrypt the password
+      const bcryptPass = await bcrypt.hash(password, 10);
+      db.insertUser(username, role, bcryptPass);
+    }
+    res.redirect("/");
+  } catch (error) {
+    res.render("fail.ejs");
+  }
+});
 
 app.get("/admin", async (req, res) => {
   const users = await db.getUsers();
--- database.js
+++ database.js
@@ -10,20 +10,6 @@ const db = new sqlite3.Database("./users.db", (err) => {
   console.log("connected to library db in sqlite");
 });
 
-const insertUser = () => {
-  /*  db.run(
-    `INSERT INTO USERS (name, email, username, password) VALUES(?,?,?,?)`,
-    [name, email, password, bcrypt],
-    function (err) {
-      if (err) {
-        return console.log("errror" + err.message);
-      }
-      // get the last insert id
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
-    }
-  ); */
-};
-
 const getUsers = async (username) => {
   const queryBook = "SELECT * FROM users";
   return new Promise((resolve, reject) => {
@@ -35,7 +21,32 @@ const getUsers = async (username) => {
   });
 };
 
+const getUser = async (username) => {
+  const queryBook = "SELECT * FROM USERS WHERE username LIKE ?";
+  return new Promise((resolve, reject) => {
+    db.each(queryBook, [username], (err, row) => {
+      if (err) reject();
+      else resolve(row);
+    });
+  });
+};
+
+const insertUser = (name, role, encryptedPAss) => {
+  db.run(
+    `INSERT INTO users (name, role, password) VALUES(?,?,?)`,
+    [name, role, encryptedPAss],
+    function (err) {
+      if (err) {
+        return console.log("error" + err.message);
+      }
+      // get the last insert id
+      console.log(`A row has been inserted with row id ${this.lastID}`);
+    }
+  );
+};
+
 module.exports = {
   insertUser,
   getUsers,
+  getUser,
 };
