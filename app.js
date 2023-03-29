6d00db7e944addd389d02d8cb41c9e99f03c4a98
Murwan Eisa <eisa.murwan@gmail.com>
2023-03-29 15:30:33 +0200

    finishing rendering user into admin page

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
-db.insertUser();
+/* const filePath = path.join(__dirname, "./views");
+app.set("views", filePath);
+console.log("first", filePath); */
+const PORT = 3000;
+
+app.get("/admin", async (req, res) => {
+  const users = await db.getUsers();
+  console.log("users", users);
+  res.render("admin.ejs", { users });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listen on port ${PORT}`);
+});
--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+const { raw } = require("express");
+
 const sqlite3 = require("sqlite3").verbose();
 
 //connect to the database
@@ -9,9 +11,31 @@ const db = new sqlite3.Database("./users.db", (err) => {
 });
 
 const insertUser = () => {
-  console.log("insert user");
+  /*  db.run(
+    `INSERT INTO USERS (name, email, username, password) VALUES(?,?,?,?)`,
+    [name, email, password, bcrypt],
+    function (err) {
+      if (err) {
+        return console.log("errror" + err.message);
+      }
+      // get the last insert id
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+    }
+  ); */
+};
+
+const getUsers = async (username) => {
+  const queryBook = "SELECT * FROM users";
+  return new Promise((resolve, reject) => {
+    db.all(queryBook, [], (err, row) => {
+      if (err) reject();
+      else resolve(row);
+      //console.log("raw", row);
+    });
+  });
 };
 
 module.exports = {
   insertUser,
+  getUsers,
 };
